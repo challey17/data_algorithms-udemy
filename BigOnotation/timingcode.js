df97345ddd67c1f67ac79adad589f3abf4f315aa
Chayce Knaub <chknaub@yahoo.com>
2020-10-25 00:04:56 -0400

    bigO notation shiiiiiit

--- BigOnotation/timingcode.js
+++ BigOnotation/timingcode.js
@@ -1,6 +1,9 @@
+//slides https://cs.slides.com/colt_steele/big-o-notation#/21
+
 //Write a function that calculates the sum of all numbers
 // from 1 up to and including some number n
 
+//0(n) linear time, the code runtime increases proportionally to size of input
 function addUpTo(n) {
   //accumulator
   let total = 0;
@@ -12,6 +15,7 @@ function addUpTo(n) {
 
 console.log(addUpTo(6));
 
+// O(1) constant time, regardless of input, code runtime is pretty much constant
 function addUpTo2(n) {
   return (n * (n + 1)) / 2;
 }
